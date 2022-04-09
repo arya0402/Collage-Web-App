1d96e0b3b08053dc9912e777d93596205b42c3f7
Aryadolphin <78508957+arya0402@users.noreply.github.com>
2022-04-09 13:03:07 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ var recognition = new SpeechRecognition();
 var content;
 
 function start() {
-    recognition.start();   
+    recognition.start();
 }
 
 recognition.onresult = function (event) {
     console.log(event);
     content = event.results[0][0].transcript.toLowerCase();
     console.log(content);
-    if(content == "selfie") {
+    if (content == "selfie") {
         speak();
     }
 }
@@ -28,3 +28,34 @@ function speak() {
     }, 5000);
     Webcam.attach(camera);
 }
+
+var img_id = "selfie1";
+
+
+function setTimeout(function() {
+    img_id = "selfie1";
+    take_snapshot();
+    speak_data = "Taking your selfie in 10 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+}, 10000);
+
+function take_snapshot() {
+    console.log(img_id);
+
+    Webcam.snap(function (data_uri) {
+        if (img_id == "selfie1") {
+            document.getElementById("result1").innerHTML = "<img id='selfie1' src= '" + data_uri + "'/>";
+        }
+
+        if (img_id == "selfie2") {
+            document.getElementById("result2").innerHTML = "<img id='selfie2' src= '" + data_uri + "'/>";
+        }
+
+        if (img_id == "selfie3") {
+            document.getElementById("result3").innerHTML = "<img id='selfie3' src= '" + data_uri + "'/>";
+        }
+    });
+}
+
